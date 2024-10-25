85bb8f3b58096055e7af5c102d00b06540485cb6
enesagu <97469455+enesagu@users.noreply.github.com>
2024-10-25 16:13:54 +0300

    Header and logo style design
    Dark and light mod added.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,67 @@
-import React from 'react'
-import '../css/Header.css'
+import React, { useState } from "react";
+import "../css/Header.css";
+import { FaTruckRampBox } from "react-icons/fa6";
+import { CiLight } from "react-icons/ci";
+import { WiMoonFull } from "react-icons/wi";
 
 function Header() {
+  const [theme, setTheme] = useState(false);
+
+  const changeTheme = () => {
+    const root = document.getElementById("root");    
+    
+    setTheme((prevTheme) => !prevTheme);
+
+    if(theme)
+    {
+      root.style.backgroundColor = "black";
+      root.style.color = "#fff";
+
+    }
+    else{
+      root.style.backgroundColor = "#fff";
+      root.style.color = "black";
+    }
+    
+  };
+
   return (
-    <div style={{display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"space-betwen"}}>
-        <div className='logo-div'>
-            <img className='logo' src='../public/favicon2.png'/>
-            <p className='logo-text'>FireNanit</p>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between", // Typo düzeltildi (betwen -> between)
+      }}
+    >
+      <div className="logo-div">
+      <img
+          className="logo"
+          src={theme ? "../public/favicon-black.png" : "../public/favicon-white.png"} 
+          alt="Logo"
+        />        <p className="logo-text">FireNanit</p>
+      </div>
+
+      <div
+        
+        style={{ display: "flex", alignItems: "center" }}
+      >
+        <input  className="search-input" type="text" placeholder="Search anyThing" />
+
+        <div
+          
+          style={{ cursor: "pointer", marginLeft: "10px" }}
+        >
+          {theme ? <WiMoonFull onClick={changeTheme} className="icons"/> : <CiLight onClick={changeTheme} className="icons"/>
+          
+            // buraya 
+          }
+
+          <FaTruckRampBox className="icons"/>
         </div>
-
-        <div className='search-input'>
-            <input type='text' placeholder='Search anyThing'/>
-        </div>
-
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
